17212205a87b5eb6032ad91d70e9f5f4f409206f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-05-02 18:03:02 +0200

    Parallel routes

---
+++ src/app/complex-dashboard/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+// Eash slot pass to layout as a prop so I can destructure it
+export default function ComplexLayout({
+  children,
+  users,
+  revenue,
+  notifications,
+}: {
+  children: React.ReactNode;
+  users: React.ReactNode;
+  revenue: React.ReactNode;
+  notifications: React.ReactNode;
+}) {
+  return (
+    <div className="m-5">
+      {children}
+      <div className="flex">
+        <div className="flex flex-col">
+          <div>{users}</div>
+          <div>{revenue}</div>
+          <div>{notifications}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/complex-dashboard/page.tsx
@@ -0,0 +1,9 @@
+const ComplexDashboard = () => {
+    return (
+      <>
+        <h1>Complex Dashboard</h1>
+      </>
+    );
+};
+
+export default ComplexDashboard;
