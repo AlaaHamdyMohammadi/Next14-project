ab704fa9cf98d21a31007aed5b2391f5f7da34f4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-05-04 01:39:33 +0200

    patch request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -4,6 +4,20 @@ export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-    const comment = comments.find(comment => comment.id === parseInt(params.id))
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
   return Response.json(comment);
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { text } = await request.json();
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[index].text = text;
+  return Response.json(comments[index]);
 }
