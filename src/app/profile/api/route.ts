8a8c8206c9c62a2065969f8fe3f24685013a9e81
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-05-04 17:39:11 +0200

    Cookies in route handler

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 
@@ -6,14 +6,18 @@ export async function GET(request: NextRequest){
     const requestHeaders = new Headers(request.headers);
     const headerList = headers();
     
+    cookies().set("results", "20");
+    const theme = request.cookies.get('theme');
+
+
     console.log("headerList : ", headerList.get('Authorization'));
     console.log("requestHeaders : ", requestHeaders.get('Authorization'));
     
-
     
     return new Response('<h1>Profile API</h1>', {
         headers: {
-            "Content-Type": "text/html"
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     });
 }
